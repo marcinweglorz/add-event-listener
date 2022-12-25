8f656411fb51fc9fec19f498a152af8e6154ea19
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2022-12-25 17:40:29 +0100

    connect button with css animation

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
-const redBtn = document.querySelector("#btn1");
-const blueBtn = document.querySelector(".btn2");
-const square = document.querySelector(".color");
-const removeColorBtn = document.querySelector(".remove-color");
+const redBtn = document.querySelector('#btn1')
+const blueBtn = document.querySelector('.btn2')
+const square = document.querySelector('.color')
+const removeColorBtn = document.querySelector('.remove-color')
+const animationBtn = document.querySelector('.btn3')
+const animationSquare = document.querySelector('.animsquare')
 
 function redColor() {
-  square.classList.add("red");
-  square.classList.remove("blue");
+	square.classList.add('red')
+	square.classList.remove('blue')
 }
 
 function blueColor() {
-  square.classList.add("blue");
-  square.classList.remove("red");
+	square.classList.add('blue')
+	square.classList.remove('red')
 }
 
 function deleteColor() {
-  square.classList.remove("blue");
-  square.classList.remove("red");
+	square.classList.remove('blue')
+	square.classList.remove('red')
 }
 
-removeColorBtn.addEventListener("click", deleteColor);
-redBtn.addEventListener("click", redColor);
-blueBtn.addEventListener("click", blueColor);
+function movingSquare() {
+	animationSquare.classList.toggle('animsquare')
+}
+
+removeColorBtn.addEventListener('click', deleteColor)
+redBtn.addEventListener('click', redColor)
+blueBtn.addEventListener('click', blueColor)
+animationBtn.addEventListener('click', movingSquare)
