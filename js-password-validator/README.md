e64bd35c78b444e24913276f2cf03a98fc2a210c
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-02-25 12:40:36 +0100

    add basic password validator in javaScript

---
+++ js-password-validator/README.md
@@ -0,0 +1,5 @@
+# JavaScript exercise #3
+
+Simple password validator in JavaScript using RegExp to check values of letters, special characters and numbers.
+
+
---
+++ js-password-validator/script.js
@@ -0,0 +1,36 @@
+const pass = document.querySelector('#password')
+const p = document.querySelector('.passinfo')
+const letters = /[a-z]/i
+const numbers = /[0-9]/
+const special = /[!@#$%^&*()]/
+const minValue = 10
+
+const showMsg = () => {
+	if (
+		pass.value.length > minValue &&
+		pass.value.match(letters) &&
+		pass.value.match(numbers) &&
+		pass.value.match(special)
+	) {
+		p.textContent = 'Masz bardzo dobre hasło!👍🏻'
+		p.style.color = 'lime'
+	} else if (pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)) {
+		p.textContent = 'Masz dobre hasło!🙂'
+		p.style.color = 'gold'
+	} else {
+		p.textContent = 'Masz słabe hasło!🙂'
+		p.style.color = 'tomato'
+	}
+}
+
+const checkPassword = () => {
+	if (pass.value !== '') {
+		showMsg
+	} else {
+		p.textContent = 'Nie podałeś hasła...'
+		p.style.color = ''
+	}
+}
+
+pass.addEventListener('keyup', showMsg)
+pass.addEventListener('keyup', checkPassword)
