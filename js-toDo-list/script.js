1306cf5f4f5926b91fe719244b3591777a7078fd
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-02-28 21:41:21 +0100

    created listener for DOM content and added main function

---
+++ js-toDo-list/script.js
@@ -0,0 +1,20 @@
+let todoInput
+let errorInfo
+let addBtn
+let ulList
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
+}
+
+const prepareDOMEvents = () => {}
+
+document.addEventListener('DOMContentLoaded', main)
