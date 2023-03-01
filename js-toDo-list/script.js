ee08d7c4aba4dce9aca1b3f4976553423ffff4cc
Marcin Węglorz <marcinweglorz.dev@gmail.com>
2023-03-01 20:32:12 +0100

    added buttons with classes to new tasks

--- js-toDo-list/script.js
+++ js-toDo-list/script.js
@@ -23,12 +23,35 @@ const addNewTodo = () => {
 	if (todoInput.value !== '') {
 		newTodo = document.createElement('li')
 		newTodo.textContent = todoInput.value
+
+		createToolsArea()
+		
 		ulList.append(newTodo)
 		todoInput.value = ''
 		errorInfo.textContent = ''
 	} else {
 		errorInfo.textContent = 'Wpisz treść zadania!'
 	}
+}
+
+const createToolsArea = () => {
+	const toolsPanel = createElement('div')
+	toolsPanel.classList.add('tools')
+	newTodo.append(toolsPanel)
+
+	const completeBtn = createElement('button')
+	completeBtn.classList.add('complete')
+	completeBtn.innetHTML = '<i class="fas fa-check"></i>'
+
+	const editBtn = createElement('button')
+	editBtn.classList.add('edit')
+	editBtn.textContent = 'EDIT'
+
+	const deleteBtn = createElement('button')
+	deleteBtn.classList.add = 'delete'
+	deleteBtn.innetHTML = '<i class="fas fa-times"></i>'
+
+	toolsPanel.append(completeBtn, editBtn, deleteBtn)
 }
 
 document.addEventListener('DOMContentLoaded', main)
